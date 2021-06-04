b3ab2bbb9b8a344476c0fc99e980b21cce01e0da
martin visca <martin.visca@protonmail.com>
2021-06-04 10:40:08 +0200

    Add root

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,5 @@
 class ListsController < ApplicationController
-  # before_action :set_list, only: [:show]
+  before_action :set_list, only: [:show]
 
   def index
     @lists = List.all
@@ -9,8 +9,26 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
 
+  def new
+    @list = List.new
+  end
 
-  # def set_list
-  #   @list = List.find(params[:id])
-  # end
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to new_list_bookmark_path(@list)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def set_list
+      @list = List.find(params[:id])
+    end
+
+    def list_params
+      params.require(:list).permit(:name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    resources :movies, only: [ :index, :show ]
 
-    resources :lists, only: [ :index, :show] do
-       resources :bookmarks, only: [ :new, :create]
-    end
+  root to: 'lists#index'
 
+  # resources :movies, only: [ :index, :show ]
+
+  resources :lists, only: [:new, :create, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
 end
